3b6a1a3b2d38224fc195fc17ec177eed666f14eb
SandhyaS14 <85371666+SandhyaS14@users.noreply.github.com>
2021-12-06 07:07:36 -0500

    Add files via upload

--- eye.js
+++ eye.js
@@ -1,3 +1,8 @@
 function back() {
     window.location = "index.html";
 }
+
+function next() {
+    window.alert("You are about to enter the main webpage. You will not be able to return to these pages until the very end.");
+    window.location = "module.html";
+}
