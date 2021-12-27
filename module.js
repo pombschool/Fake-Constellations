a3d9c3e9d0ffc13f249212a48591b719f7381965
SandhyaS14 <85371666+SandhyaS14@users.noreply.github.com>
2021-12-27 17:50:35 -0500

    Add files via upload

---
+++ module.js
@@ -0,0 +1,18 @@
+function help() {
+    user = document.getElementById("key").value;
+    if(user == "Goodwin"){
+        localStorage.setItem("user", "Mrs. Goodwin");
+        document.getElementById("key").value = "";
+        window.location = "Help.html";
+    }
+    else if(user == "Shreyas"){
+        localStorage.setItem("user", "Shreyas");
+        document.getElementById("key").value = "";
+        window.location = "Help.html";
+    }
+    else {
+        document.getElementById("key").style = "background-color: red;";
+        document.getElementById("body").style = "border-color: red;";
+        document.getElementById("key").value = "";
+    };
+}
