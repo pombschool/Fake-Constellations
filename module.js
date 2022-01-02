12663ae0c728c8414896765b42779ffa534ce471
SandhyaS14 <85371666+SandhyaS14@users.noreply.github.com>
2022-01-02 17:22:33 -0500

    Add files via upload

--- module.js
+++ module.js
@@ -1,3 +1,17 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCaG3h_ucaMzyVlpGXB-6nNSk8eGPkx2rs",
+    authDomain: "constellations-f8f62.firebaseapp.com",
+    databaseURL: "https://constellations-f8f62-default-rtdb.firebaseio.com",
+    projectId: "constellations-f8f62",
+    storageBucket: "constellations-f8f62.appspot.com",
+    messagingSenderId: "918076456711",
+    appId: "1:918076456711:web:ad89d6708c266c69c9cfa2",
+    measurementId: "G-RS6VJFRFWG"
+  };
+  
+  // Initialize Firebase
+  var app = initializeApp(firebaseConfig);
+
 function help() {
     user = document.getElementById("key").value;
     if(user == "Goodwin"){
